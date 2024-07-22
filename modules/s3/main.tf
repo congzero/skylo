deb62e917626c15842837088a254d0fda9d9d830
congzero <shqkdnsem@naver.com>
2024-07-22 11:25:17 +0900

    Add files via upload

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -41,14 +41,3 @@ data "aws_iam_policy_document" "s3_bucket_policy" {
   }
 }
 
-output "bucket_website_url" {
-  value = aws_s3_bucket.website_bucket.website_endpoint
-}
-
-output "bucket_arn" {
-  value = aws_s3_bucket.website_bucket.arn
-}
-
-output "bucket_regional_domain_name" {
-  value = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-}
