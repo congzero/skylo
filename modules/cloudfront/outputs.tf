5fe38741c452d8acf0d1b0cf7923dd6990207746
congzero <shqkdnsem@naver.com>
2024-07-22 11:23:31 +0900

    Add files via upload

---
+++ modules/cloudfront/outputs.tf
@@ -0,0 +1,12 @@
+output "domain_name" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+output "hosted_zone_id" {
+  value = aws_cloudfront_distribution.cdn.hosted_zone_id
+}
+
+
+output "cloudfront_arn" {
+  value = aws_cloudfront_distribution.cdn.arn
+}
